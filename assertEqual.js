e6ef7e1692d084e1bf3604de0814b5144b7c58d0
Atkinss1 <jordanatkins@live.com>
2023-09-19 20:59:05 -0600

    Added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+// Function to compare two parameters
+const assertEqual = (actual, expected) => {
+  // if both parameters are equal to one another it will pass
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: ", actual, "===", expected);
+    // else it will print fail
+  } else {
+    console.log("🛑🛑🛑 Assertion failed: ", actual, '!==', expected);
+  }
+};
+
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("Hello", "Hi");
+assertEqual(33, 33);
+assertEqual(234, 12);
