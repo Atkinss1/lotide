c856bae310d8e1b7ebcfcc1de1813664153a5dbd
Atkinss1 <jordanatkins@live.com>
2023-09-20 18:11:01 -0600

    Added assertEqual function and head function

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@
 const assertEqual = (actual, expected) => {
   // if both parameters are equal to one another it will pass
   if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: ", actual, "===", expected);
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
     // else it will print fail
   } else {
-    console.log("🛑🛑🛑 Assertion failed: ", actual, '!==', expected);
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
