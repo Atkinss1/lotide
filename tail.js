782f9e379a2cd936f24d65cd6b8d5be271e39f3e
Atkinss1 <jordanatkins@live.com>
2023-09-20 18:56:10 -0600

    Added tail function with test cases

--- tail.js
+++ tail.js
@@ -10,5 +10,19 @@ const assertEqual = (actual, expected) => {
 };
 
 const tail = (arr) => {
-  const newArr = arr.map((i) => )
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 };
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
