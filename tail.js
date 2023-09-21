e0913f7be73701f2f86756f25e876d3c51812725
Atkinss1 <jordanatkins@live.com>
2023-09-20 18:34:42 -0600

    quick check up

---
+++ tail.js
@@ -0,0 +1,14 @@
+// Function to compare two parameters
+const assertEqual = (actual, expected) => {
+  // if both parameters are equal to one another it will pass
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+    // else it will print fail
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (arr) => {
+  const newArr = arr.map((i) => )
+};
