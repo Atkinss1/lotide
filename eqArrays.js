6c8687d51d7e1f08a0af671901cb4069a2b6453d
Atkinss1 <jordanatkins@live.com>
2023-09-21 21:39:33 -0600

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -13,12 +13,21 @@ const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
-    return arr1.toString() === arr2.toString();
+    for (let a = 0; a < arr1.length; a++) {
+      if (arr1[a] !== arr2[a]) {
+        return false;
+      }
+    }
+    return true;
   }
 };
+  
+
+
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 1, 2], [2, 3, 5, 7], true));
+assertEqual(eqArrays([1, 1, 2], [2, 3, 5, 7]), true);
 assertEqual(eqArrays([3, 2, 3], [3, 2, 3]), true);
 assertEqual(eqArrays([1, 2], [1, 2, 3]), true);
 assertEqual(eqArrays([1, null, 3], [1, null, 3]), true);
--- head.js
+++ head.js
@@ -20,3 +20,4 @@ const head = (arr) => {
 assertEqual(head([5, 6, 7]),5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]));
+
