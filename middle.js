85912693daea805e324f9ccbdee6a7895e31cb0f
Atkinss1 <jordanatkins@live.com>
2023-09-22 22:03:25 -0600

    currently working on middle function

--- middle.js
+++ middle.js
@@ -18,3 +18,31 @@ const assertArraysEqual = (arr1, arr2) => {
     console.log("The comparisons are not equal");
   }
 };
+
+const middle = (arr) => {
+  let middleIndex = [];
+  if (arr.length < 3) {
+    return middleIndex;
+  }
+  if (arr.length % 2 === 1) {
+    let middle = Math.floor(arr.length) / 2 | 0;
+    console.log(middle);
+    middleIndex.push(arr[middle]);
+    
+  }
+  return middleIndex;
+};
+
+console.log(middle([1, 2, 3])); // [2]
+console.log(middle([1, 2, 3, 4, 5])); // [3]
+console.log(middle([1, 2, 3, 4])); // [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // [3, 4]
+
+// assertArraysEqual(middle([1])); // => []
+// assertArraysEqual(middle([1, 2])); // => []
+
+// assertArraysEqual(middle([1, 2, 3])); // => [2]
+// assertArraysEqual(middle([1, 2, 3, 4, 5])); // => [3]
+
+// assertArraysEqual(middle([1, 2, 3, 4]));// => [2, 3]
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
