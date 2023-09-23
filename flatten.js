f91ce63dddec8bffa7f314b44b4c3a3b672d1972
Atkinss1 <jordanatkins@live.com>
2023-09-22 20:38:26 -0600

    Added flatten function that will take in an array, check if there are any nested arrays and return a flattened array.

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let a = 0; a < arr1.length; a++) {
+      if (arr1[a] !== arr2[a]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log("The comparisons are equal");
+  } else {
+    console.log("The comparisons are not equal");
+  }
+};
+
+const flatten = (arr) => {
+  let flattenArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!Array.isArray(arr[i])) {
+      flattenArr.push(arr[i]);
+    } else {
+      arr[i].forEach((element) =>
+        flattenArr.push(element));
+    }
+  }
+  return flattenArr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
