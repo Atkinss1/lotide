97cce59cadc34fe75327ae25976b5915c724f3e0
Atkinss1 <jordanatkins@live.com>
2023-09-24 20:33:41 -0600

    added middleIndex function to grab the middle indexes of an array

--- middle.js
+++ middle.js
@@ -18,15 +18,19 @@ const assertArraysEqual = (arr1, arr2) => {
     console.log("The comparisons are not equal");
   }
 };
-
+// function called middle, that will return the single middle index of an array if it is odd, and the 2 middle indexes of an array
 const middle = (arr) => {
+  // created an empty to array that will be returned if the array is 2 or less
   let emptyArr = [];
   if (arr.length < 3) {
     return emptyArr;
   }
+  // finding our middle index value by dividing the array in half
   let middleIndex = Math.floor(arr.length / 2);
+  // if our array is even it will need to return two indexes
   if (arr.length % 2 === 0) {
     return [arr[middleIndex - 1], arr[middleIndex]];
+    // if our array is odd, it will return one index
   } else {
     return arr[middleIndex];
   }
