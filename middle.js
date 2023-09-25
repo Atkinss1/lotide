8eaa98b167630089459d5f643edb3a031e3fab2b
Atkinss1 <jordanatkins@live.com>
2023-09-24 20:35:24 -0600

    commented out eqArrays and assertArraysEqual functions

--- middle.js
+++ middle.js
@@ -1,23 +1,23 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let a = 0; a < arr1.length; a++) {
-    if (arr1[a] !== arr2[a]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let a = 0; a < arr1.length; a++) {
+//     if (arr1[a] !== arr2[a]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log("The comparisons are equal");
-  }
-  if (!eqArrays(arr1, arr2)) {
-    console.log("The comparisons are not equal");
-  }
-};
+// const assertArraysEqual = (arr1, arr2) => {
+//   if (eqArrays(arr1, arr2)) {
+//     console.log("The comparisons are equal");
+//   }
+//   if (!eqArrays(arr1, arr2)) {
+//     console.log("The comparisons are not equal");
+//   }
+// };
 // function called middle, that will return the single middle index of an array if it is odd, and the 2 middle indexes of an array
 const middle = (arr) => {
   // created an empty to array that will be returned if the array is 2 or less
