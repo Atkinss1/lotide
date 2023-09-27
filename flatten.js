06194550c022bcf005c1fa439403ea5eab9d1d12
Atkinss1 <jordanatkins@live.com>
2023-09-26 21:02:00 -0600

    added testcase using assertArraysEquals

--- flatten.js
+++ flatten.js
@@ -30,4 +30,4 @@ const flatten = (arr) => {
   return flattenArr;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])); // => [1, 2, 3, 4, 5, 6]
