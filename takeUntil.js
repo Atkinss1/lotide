27a0dbb8d3dbd6a1dbfc0a1fbbfa715a0444815a
Atkinss1 <jordanatkins@live.com>
2023-09-28 20:38:54 -0600

    added assertArrayEqual tests

--- takeUntil.js
+++ takeUntil.js
@@ -36,9 +36,11 @@ const takeUntil = function (array, callback) {
 };
 
 
-console.log(takeUntil(data1, x => x < 0));
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 console.log('---');
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
