73f1dc45ef69aed96483d8095f6bea0f65820862
Atkinss1 <jordanatkins@live.com>
2023-10-10 20:26:56 -0600

    refactoring test functions

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-// const eqArrays = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   for (let a = 0; a < arr1.length; a++) {
-//     if (arr1[a] !== arr2[a]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = (arr1, arr2) => {
-//   if (eqArrays(arr1, arr2)) {
-//     console.log("The comparisons are equal");
-//   }
-//   if (!eqArrays(arr1, arr2)) {
-//     console.log("The comparisons are not equal");
-//   }
-// };
 // function called middle, that will return the single middle index of an array if it is odd, and the 2 middle indexes of an array
 const middle = (arr) => {
   // created an empty to array that will be returned if the array is 2 or less
@@ -32,11 +12,8 @@ const middle = (arr) => {
     return [arr[middleIndex - 1], arr[middleIndex]];
     // if our array is odd, it will return one index
   } else {
-    return arr[middleIndex];
+    return [arr[middleIndex]];
   }
 };
 
-console.log(middle([1, 2, 3])); // [2]
-console.log(middle([1, 2, 3, 4, 5])); // [3]
-console.log(middle([1, 2, 3, 4])); // [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // [3, 4]
+module.exports = middle;
