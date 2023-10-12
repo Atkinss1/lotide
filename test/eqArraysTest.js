eea9f8acadd185d8b9af41caea9c4421c16b3f60
Atkinss1 <jordanatkins@live.com>
2023-10-12 17:21:52 -0600

    add chai testing to eqArraysTest file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,16 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 1, 2], [2, 3, 5, 7]), false);
-assertEqual(eqArrays([3, 2, 3], [3, 2, 3]), true);
-assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
-assertEqual(eqArrays([1, null, 3], [1, null, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe('#eqArrays', () => {
+  it('should return true if arrays are identical', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it('should return false if arrays are not identical', () => {
+    assert.isFalse(eqArrays([1, 1, 2], [2, 3, 5, 7]));
+  });
+
+  it('should return false if indexs are not identical', () => {
+    assert.isFalse(eqArrays([1, 2, 3], [2, 1, 3]));
+  });
+});
