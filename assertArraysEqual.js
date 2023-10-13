3f6c8b56151b04b89502597a5d4bfa3a7aacca0b
Atkinss1 <jordanatkins@live.com>
2023-10-12 18:28:41 -0600

    add function description

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,6 @@
 const eqArrays = require('./eqArrays.js');
 
+// compares two arrays and prints out if they are equal or not
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
     console.log("The comparisons are equal");
