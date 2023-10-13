f4f46eb2211270e7df7756c057e87f1c9af5fc2e
Atkinss1 <jordanatkins@live.com>
2023-10-12 18:41:07 -0600

    add function description

--- map.js
+++ map.js
@@ -1,4 +1,6 @@
-const map = function (array, callback) {
+// function will take in an array and a callback. It will return a new array based on the results of the callback
+
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
