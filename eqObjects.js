70a5e6af7072700c404525aeefbd03227327e186
Atkinss1 <jordanatkins@live.com>
2023-10-12 18:34:05 -0600

    add description of function

--- eqObjects.js
+++ eqObjects.js
@@ -2,9 +2,7 @@ const eqArrays = require('./eqArrays');
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
-  //expected array = [color, size]
   const array1 = Object.keys(object1);
-  //expected array = [size, color]
   const array2 = Object.keys(object2);
   
   if (array1.length !== array2.length) {
@@ -23,3 +21,8 @@ const eqObjects = function(object1, object2) {
 };
 
 module.exports = eqObjects;
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
--- findKey.js
+++ findKey.js
@@ -1,3 +1,5 @@
+// function will take in an object and a call back. It will scan the object and return the first key when the callback returns a truthy value
+
 const findKey = function(object, callback) {
   for (let item in object) {
     if (callback(object[item])) {
