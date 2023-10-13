ec564bb1f28bb94a73f363282ab34d7cc6145072
Atkinss1 <jordanatkins@live.com>
2023-10-12 18:28:08 -0600

    add comments to describe function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,6 +1,8 @@
 const { inspect } = require('util');
 const eqObjects = require('./eqObjects');
 
+// takes in two objects and prints out pass or fail if they are equal or not equal
+
 const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) {
     console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
