77e920363edf8b45232dd7d853303a048a691476
Atkinss1 <jordanatkins@live.com>
2023-10-12 18:05:53 -0600

    add comments to clarify function usage

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,4 @@
+// this function takes in an array of items and an object that determines what you want counted, it will return an object containing counts of everything that the object parameter listed
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
 
